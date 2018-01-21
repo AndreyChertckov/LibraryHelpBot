b022db4cca050476d76e627f4c0135d02a62fd0b
Rinatti <31774217+Rinatti@users.noreply.github.com>
2018-01-21 03:16:30 -0800

    Update Testing.py

--- DataBase/Testing.py
+++ DataBase/Testing.py
@@ -4,6 +4,5 @@ from UsersAndDocumentObjects.Librarian import Librarian
 from UsersAndDocumentObjects.Patron import Patron
 from UsersAndDocumentObjects.Patron import PatronType
 def main():
-    p1=Patron("Vasya","UKRAIN","student",1,8999,[],[],2)
-    p1.get_info()
+    
 main()
