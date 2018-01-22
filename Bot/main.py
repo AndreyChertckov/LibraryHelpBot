699d4b6571443fe90b4f7d7282e9e354499809d2
Animatory <shulz134700@gmail.com>
2018-01-22 21:15:33 +0300

    add bot

---
+++ Bot/main.py
@@ -0,0 +1,19 @@
+import telegram
+from telegram.ext import Updater, CommandHandler
+import logging
+
+token = '537025892:AAHqwqWaGEKdb4bBBQ9CJlKGa8mAqz7fElI'
+
+bot = telegram.Bot(token=token)
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
