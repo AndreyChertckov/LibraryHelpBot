06bc4087ea93721e5d984b5c7f03207b534afce2
Animatory <shulz134700@gmail.com>
2018-01-31 12:38:24 +0300

    Select_label added,BDTables deleted

--- DataBase/UsersAndDocumentObjects/JournalArticle.py
+++ DataBase/UsersAndDocumentObjects/JournalArticle.py
@@ -1,6 +1,8 @@
 from DataBase.UsersAndDocumentObjects.BaseDoc import BaseDoc
+
+
 class JournalArticle(BaseDoc):
-    def __init__(self, name,author,journal_name,journal_publisher,id, count, free_count, price):
+    def __init__(self, name, author, journal_name, journal_publisher, id, count, free_count, price):
         BaseDoc.__init__(self, id, author, name, count, free_count, price, "MEDIA")
-        self.journal_name=journal_name
-        self.journal_publisher=journal_publisher
+        self.journal_name = journal_name
+        self.journal_publisher = journal_publisher
