34c3c396b21623e72cf3010c1d2f9ad41e4b2da0
Animatory <shulz134700@gmail.com>
2018-02-11 16:45:28 +0300

    Formatting

--- DataBase/UsersAndDocumentObjects/BaseDoc.py
+++ DataBase/UsersAndDocumentObjects/BaseDoc.py
@@ -3,13 +3,12 @@
 class BaseDoc:
     # Initialization
     # Params:id (int) , authors , title, count, free_count(available count),price,type
-    def __init__(self,id,authors, title, count, free_count, price, type,keywords):
-        self.id=id
+    def __init__(self, id, authors, title, count, free_count, price, type, keywords):
+        self.id = id
         self.authors = authors
         self.name = title
         self.count = count
         self.free_count = free_count
         self.price = price
         self.type = type
-        self.keywords=keywords
-
+        self.keywords = keywords
