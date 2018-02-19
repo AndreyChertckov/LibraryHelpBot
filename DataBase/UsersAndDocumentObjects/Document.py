89f2d1f6a8e970138377d140e166dfb882626d80
Rinat <rinat.babichev@gmail.com>
2018-02-19 20:24:10 +0300

    BD repaired

--- DataBase/UsersAndDocumentObjects/Document.py
+++ DataBase/UsersAndDocumentObjects/Document.py
@@ -5,5 +5,5 @@ from DataBase.UsersAndDocumentObjects.BaseDoc import BaseDoc
 # Inherits from BaseDoc and adds new attribute:description
 class Document(BaseDoc):
     def __init__(self,name, description, author, count, free_count, price, best_seller, keywords,id=0):
-        BaseDoc.__init__(self, id, author, name, count, free_count, price, "BOOK", keywords,best_seller)
+        BaseDoc.__init__(self,author, name, count, free_count, price, "BOOK", keywords,best_seller,id)
         self.description = description
