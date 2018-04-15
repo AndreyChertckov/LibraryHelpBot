7134b0af7c8deb6543e2efdef96e913fbd05f096
Vyacheslav Shults <shulz134700@gmail.com>
2018-04-15 18:43:26 +0300

    ncghf

--- AdminSite/notification.py
+++
@@ -1,7 +0,0 @@
-class Notification:
-    
-    def __init__(self, bot):
-        self.bot = bot
-    
-    def send_message(self, chat_id,message):
-        self.bot.send_message(chat_id=chat_id, text=message)
